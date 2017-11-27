225dce8956b081894eb4309a9eefc79ab411e7e5
yangxiu <yangxiu@cnicg.cn>
2017-11-27 11:31:17 +0800

    uplify img and install vue-awesome-swiper

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,23 +3,23 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const _00023a46 = () => import('..\\pages\\index.vue' /* webpackChunkName: "pages\\index" */).then(m => m.default || m)
-const _202de86a = () => import('..\\pages\\news\\index.vue' /* webpackChunkName: "pages\\news\\index" */).then(m => m.default || m)
-const _132a3fa6 = () => import('..\\pages\\example\\index.vue' /* webpackChunkName: "pages\\example\\index" */).then(m => m.default || m)
-const _3b1f1268 = () => import('..\\pages\\function\\service.vue' /* webpackChunkName: "pages\\function\\service" */).then(m => m.default || m)
-const _8e008234 = () => import('..\\pages\\us\\e-room.vue' /* webpackChunkName: "pages\\us\\e-room" */).then(m => m.default || m)
-const _18222bb5 = () => import('..\\pages\\us\\platform-intro.vue' /* webpackChunkName: "pages\\us\\platform-intro" */).then(m => m.default || m)
-const _461a019d = () => import('..\\pages\\function\\security.vue' /* webpackChunkName: "pages\\function\\security" */).then(m => m.default || m)
-const _b35ac7e8 = () => import('..\\pages\\business\\b-intro.vue' /* webpackChunkName: "pages\\business\\b-intro" */).then(m => m.default || m)
-const _7aab4781 = () => import('..\\pages\\method\\supermarket.vue' /* webpackChunkName: "pages\\method\\supermarket" */).then(m => m.default || m)
-const _9f2c98ee = () => import('..\\pages\\business\\recruit.vue' /* webpackChunkName: "pages\\business\\recruit" */).then(m => m.default || m)
-const _83321c94 = () => import('..\\pages\\function\\channel.vue' /* webpackChunkName: "pages\\function\\channel" */).then(m => m.default || m)
-const _1eda6dd9 = () => import('..\\pages\\function\\market.vue' /* webpackChunkName: "pages\\function\\market" */).then(m => m.default || m)
-const _500330b5 = () => import('..\\pages\\method\\rfid.vue' /* webpackChunkName: "pages\\method\\rfid" */).then(m => m.default || m)
-const _37d0343e = () => import('..\\pages\\us\\link-us.vue' /* webpackChunkName: "pages\\us\\link-us" */).then(m => m.default || m)
-const _5f022628 = () => import('..\\pages\\function\\c-manage.vue' /* webpackChunkName: "pages\\function\\c-manage" */).then(m => m.default || m)
-const _441a4998 = () => import('..\\pages\\function\\big-data.vue' /* webpackChunkName: "pages\\function\\big-data" */).then(m => m.default || m)
-const _17f5a6e2 = () => import('..\\pages\\_detail\\_id.vue' /* webpackChunkName: "pages\\_detail\\_id" */).then(m => m.default || m)
+const _a0208ab4 = () => import('..\\pages\\index.vue' /* webpackChunkName: "pages\\index" */).then(m => m.default || m)
+const _00a60573 = () => import('..\\pages\\news\\index.vue' /* webpackChunkName: "pages\\news\\index" */).then(m => m.default || m)
+const _9fb48af8 = () => import('..\\pages\\example\\index.vue' /* webpackChunkName: "pages\\example\\index" */).then(m => m.default || m)
+const _5b34f69e = () => import('..\\pages\\function\\service.vue' /* webpackChunkName: "pages\\function\\service" */).then(m => m.default || m)
+const _90094686 = () => import('..\\pages\\us\\e-room.vue' /* webpackChunkName: "pages\\us\\e-room" */).then(m => m.default || m)
+const _69aa028c = () => import('..\\pages\\us\\platform-intro.vue' /* webpackChunkName: "pages\\us\\platform-intro" */).then(m => m.default || m)
+const _d0bc4f18 = () => import('..\\pages\\function\\security.vue' /* webpackChunkName: "pages\\function\\security" */).then(m => m.default || m)
+const _3d990e55 = () => import('..\\pages\\business\\b-intro.vue' /* webpackChunkName: "pages\\business\\b-intro" */).then(m => m.default || m)
+const _5a1e4b8a = () => import('..\\pages\\method\\supermarket.vue' /* webpackChunkName: "pages\\method\\supermarket" */).then(m => m.default || m)
+const _47b025d2 = () => import('..\\pages\\business\\recruit.vue' /* webpackChunkName: "pages\\business\\recruit" */).then(m => m.default || m)
+const _55ad63ff = () => import('..\\pages\\function\\channel.vue' /* webpackChunkName: "pages\\function\\channel" */).then(m => m.default || m)
+const _55480e70 = () => import('..\\pages\\function\\market.vue' /* webpackChunkName: "pages\\function\\market" */).then(m => m.default || m)
+const _7e8eb4cc = () => import('..\\pages\\method\\rfid.vue' /* webpackChunkName: "pages\\method\\rfid" */).then(m => m.default || m)
+const _18485147 = () => import('..\\pages\\us\\link-us.vue' /* webpackChunkName: "pages\\us\\link-us" */).then(m => m.default || m)
+const _2206c3c3 = () => import('..\\pages\\function\\c-manage.vue' /* webpackChunkName: "pages\\function\\c-manage" */).then(m => m.default || m)
+const _2f7ab20b = () => import('..\\pages\\function\\big-data.vue' /* webpackChunkName: "pages\\function\\big-data" */).then(m => m.default || m)
+const _2290b0a6 = () => import('..\\pages\\_detail\\_id.vue' /* webpackChunkName: "pages\\_detail\\_id" */).then(m => m.default || m)
 
 
 
@@ -57,87 +57,87 @@ export function createRouter () {
     routes: [
 		{
 			path: "/",
-			component: _00023a46,
+			component: _a0208ab4,
 			name: "index"
 		},
 		{
 			path: "/news",
-			component: _202de86a,
+			component: _00a60573,
 			name: "news"
 		},
 		{
 			path: "/example",
-			component: _132a3fa6,
+			component: _9fb48af8,
 			name: "example"
 		},
 		{
 			path: "/function/service",
-			component: _3b1f1268,
+			component: _5b34f69e,
 			name: "function-service"
 		},
 		{
 			path: "/us/e-room",
-			component: _8e008234,
+			component: _90094686,
 			name: "us-e-room"
 		},
 		{
 			path: "/us/platform-intro",
-			component: _18222bb5,
+			component: _69aa028c,
 			name: "us-platform-intro"
 		},
 		{
 			path: "/function/security",
-			component: _461a019d,
+			component: _d0bc4f18,
 			name: "function-security"
 		},
 		{
 			path: "/business/b-intro",
-			component: _b35ac7e8,
+			component: _3d990e55,
 			name: "business-b-intro"
 		},
 		{
 			path: "/method/supermarket",
-			component: _7aab4781,
+			component: _5a1e4b8a,
 			name: "method-supermarket"
 		},
 		{
 			path: "/business/recruit",
-			component: _9f2c98ee,
+			component: _47b025d2,
 			name: "business-recruit"
 		},
 		{
 			path: "/function/channel",
-			component: _83321c94,
+			component: _55ad63ff,
 			name: "function-channel"
 		},
 		{
 			path: "/function/market",
-			component: _1eda6dd9,
+			component: _55480e70,
 			name: "function-market"
 		},
 		{
 			path: "/method/rfid",
-			component: _500330b5,
+			component: _7e8eb4cc,
 			name: "method-rfid"
 		},
 		{
 			path: "/us/link-us",
-			component: _37d0343e,
+			component: _18485147,
 			name: "us-link-us"
 		},
 		{
 			path: "/function/c-manage",
-			component: _5f022628,
+			component: _2206c3c3,
 			name: "function-c-manage"
 		},
 		{
 			path: "/function/big-data",
-			component: _441a4998,
+			component: _2f7ab20b,
 			name: "function-big-data"
 		},
 		{
 			path: "/:detail/:id?",
-			component: _17f5a6e2,
+			component: _2290b0a6,
 			name: "detail-id"
 		}
     ],
